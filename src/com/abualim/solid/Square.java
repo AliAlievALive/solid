8ffeee99482defdb306f99708ecdeb4a9ecc055e
AliAlievALive <madarat@mail.ru>
2022-04-28 10:05:29 +0300

    git added start Entities

---
+++ src/com/abualim/solid/Square.java
@@ -0,0 +1,13 @@
+package com.abualim.solid;
+
+public class Square {
+    private final int length;
+
+    public int getLength() {
+        return length;
+    }
+
+    public Square(int length) {
+        this.length = length;
+    }
+}
